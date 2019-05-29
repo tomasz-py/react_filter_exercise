3598df5b51d634decabc122c3fa52f3eddde25e5
Tomasz-py <tomasz.pyclik@gmail.com>
2019-05-29 13:03:16 +0200

    First step - add people list

--- src/people/People.jsx
+++ src/people/People.jsx
@@ -1,32 +1,47 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 
 // ...
 
 class People extends Component {
   static propTypes = {
-    // ...
+    people: PropTypes.array
   };
 
   // ...
 
+  renderList() {
+    let { people } = this.props;
+
+    return people.map(person => {
+      return (
+        <div className="App-box" key={person.id}>
+          {person.name}
+        </div>
+      );
+    });
+  }
+
   render() {
-    return (
-      <div>
-        {/* ... */}
-      </div>
-    );
+    return <div>{this.renderList()}</div>;
   }
 }
 
-const mapStateToProps = (state) => ({
-  // ...
-});
+const mapStateToProps = state => {
+  return { people: state.people.people };
+};
 
-const mapDispatchToProps = dispatch => bindActionCreators({
-  // ...
-}, dispatch);
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      // ...
+    },
+    dispatch
+  );
 
-export default connect(mapStateToProps, mapDispatchToProps)(People);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(People);
